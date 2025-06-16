ad2146846959c74062c1faaec39e0bc8dd32d24c
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-16 18:10:07 +0600

    added how to count time from time complexity in code

--- Basic Data Structure/Module 1/Z_Binary_Search.cpp
+++ Basic Data Structure/Module 1/Z_Binary_Search.cpp
@@ -5,19 +5,19 @@ int main()
     int n, q;
     cin >> n >> q;
     int a[n];
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)  // OPERATION COUNT : O(N)
     {
         cin >> a[i];
     }
 
     // for taking the queries
-    for (int i = 0; i < q; i++)
+    for (int i = 0; i < q; i++) // OPERATION COUNT : O(Q)
     {
         int x;
         cin >> x;
 
         int flag = 0; // for checking it is founded or not
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) // OPERATION COUNT : O(N)
         {
             if (x == a[i])
             {
@@ -28,7 +28,9 @@ int main()
 
         (flag) ? cout << "found" << endl
                : cout << "not found" << endl;
-    }
+    } // TIME COMPLEXITY FOR THIS LOOP = O(N * Q)
+
+    //GIVEN IN THE QUESTION: CONSTRAIN OF N AND Q IS 10^5 SO THE TOTAL TIME COMPLEXITY IN THE CODE IS 10^5 * 10^5 = 10^10. THE CODE MAY NEED 1000 S TO RUN THE CODE BECAUSE IN ONE SECOND 10^7 OPERATION CAN BE DONE. SO IF WE SUBMIT THE CODE WE WILL GET (TLE) Time Limit Exceed error.
 
     return 0;
 }
