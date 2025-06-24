9fe30ce8503dd8f57ee77c59b5ded933e6bdf10d
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-24 20:23:34 +0600

    added the naive approach to check equilibriuom position

---
+++ Semester 2/Basic Data Structure/Module 3.5 (practice day)/equilibrium_index.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        //let's assume that index 1 is the equilibrium index
+        int assume_equilibrium = i;
+        long long sum_bfr_eqlbrm = 0;
+        for (int j = 0; j < assume_equilibrium; j++)
+        {
+            sum_bfr_eqlbrm += a[j];
+        }
+        long long sum_aft_eqlbrm = 0;
+        for (int j = assume_equilibrium + 1; j < n; j++)
+        {
+            sum_aft_eqlbrm += a[j];
+        }
+
+        if(sum_aft_eqlbrm > sum_bfr_eqlbrm)
+        {
+            assume_equilibrium++;
+        }
+        else if(sum_aft_eqlbrm == sum_bfr_eqlbrm)
+        {
+            cout << i << endl;
+            break;
+        }
+        
+    }
+    
+    return 0;
+}
