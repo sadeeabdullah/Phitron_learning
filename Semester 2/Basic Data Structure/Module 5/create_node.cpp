72ae3a1a499098176a69ad950211fb5efcdb0c35
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-29 18:20:26 +0600

    added basic of dynamic Node

--- Semester 2/Basic Data Structure/Module 5/create_node.cpp
+++ Semester 2/Basic Data Structure/Module 5/create_node.cpp
@@ -9,20 +9,24 @@ class Node // here you can name anything you want but i am doing it as Node to m
     public:
     int val;
     Node* next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL; // at first we will keep it null then we will set
+    }
 };
 int main()
 {
-    Node a, b, c;
+    Node a(10), b(20), c(30);
 
-    a.val = 10;
-    b.val = 20;
-    c.val = 30;
+    // a.val = 10;
+    // b.val = 20;
+    // c.val = 30;
 
     // linking the node
     a.next = &b;
     b.next = &c;
-    c.next = NULL;
-
     cout << a.val << endl;
     cout << (*a.next).val << endl; // here *a.next is the dereffernced because it is the pointer of next node OUTPUT: will be the next node value or the value of b and it is 20
 
