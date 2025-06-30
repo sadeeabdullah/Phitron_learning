0012ba758ab4f823a91734d476f396d1bd109d71
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-30 20:00:21 +0600

    added how to handle if the head is null

--- Semester 2/Basic Data Structure/Module 6/insert_at_tail.cpp
+++ Semester 2/Basic Data Structure/Module 6/insert_at_tail.cpp
@@ -14,10 +14,17 @@ public:
     }
 };
 
-void insert_at_tail(Node *head, int val)
+void insert_at_tail(Node* &head, int val)
 {
     // we are creating the node we need to connect or insert at the  end
     Node *newNode = new Node(val);
+
+    //IF THE HEAD IS NULL WE CANNOT ADD VALUE BECAUSE THE IT DOES NOT FOLLOW OUR CONDITION OR WAY WE ARE TRYING TO USE WE HAVE TO HANDLE THIS WITH A ANOTHER CONDITION WE WILL SIMPLY SET THE HEAD TO THE NEW NODE
+    if(head == NULL)
+    {
+        head = newNode;
+        return; // we need to use return otherwise the whole function will be executed;
+    }
     // we need go to the last node so here we will use tmp_head node
     Node *tmp_head = head;
     while (tmp_head->next != NULL) // when condition break we will be in the last node
@@ -41,13 +48,15 @@ void print_linked_list(Node* head)
 int main()
 {
 
-    Node* head = new Node(10);
-    Node* a  = new Node(30);
-    Node* b = new Node(60);
+    // Node* head = new Node(10);
+    // Node* a  = new Node(30);
+    // Node* b = new Node(60);
+
 
     //making the connection
-    head->next = a;
-    a->next = b;
+    // head->next = a;
+    // a->next = b;
+    Node* head = NULL;
 
     insert_at_tail(head, 100);
     insert_at_tail(head, 500);
