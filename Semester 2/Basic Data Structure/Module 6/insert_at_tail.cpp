7358d6a4ef0025ab3784c269c91c8ee810ff0ae3
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-06-30 19:51:09 +0600

    added inserting node in the tail with a corner case that if the linked list is blank we couldnot add insert new node

---
+++ Semester 2/Basic Data Structure/Module 6/insert_at_tail.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *head, int val)
+{
+    // we are creating the node we need to connect or insert at the  end
+    Node *newNode = new Node(val);
+    // we need go to the last node so here we will use tmp_head node
+    Node *tmp_head = head;
+    while (tmp_head->next != NULL) // when condition break we will be in the last node
+    {
+        // now we will set the tmp_head to tmp_head->next
+        tmp_head = tmp_head->next;
+    }
+
+    tmp_head->next = newNode;
+}
+
+void print_linked_list(Node* head)
+{
+    while(head != NULL)
+    {
+        cout << head->val << endl;
+        head = head->next;
+    }
+}
+
+int main()
+{
+
+    Node* head = new Node(10);
+    Node* a  = new Node(30);
+    Node* b = new Node(60);
+
+    //making the connection
+    head->next = a;
+    a->next = b;
+
+    insert_at_tail(head, 100);
+    insert_at_tail(head, 500);
+    insert_at_tail(head, 700);
+
+    print_linked_list(head);
+    return 0;
+}
