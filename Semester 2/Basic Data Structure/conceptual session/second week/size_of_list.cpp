6fe14f6489b3432887b76674f8c4ab7578180774
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-07-01 17:34:28 +0600

    added the optimize approach to O(1) to get the size

--- Semester 2/Basic Data Structure/conceptual session/second week/size_of_list.cpp
+++ Semester 2/Basic Data Structure/conceptual session/second week/size_of_list.cpp
@@ -1,68 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int sz = 0; // we will increase the value when we add any node in the linked list
+
 class Node
 {
 public:
     int val;
-    Node* next;
+    Node *next;
 
     Node(int val)
     {
         this->val = val;
-        this->next = NULL; 
+        this->next = NULL;
     }
 };
 
-void insert_at_tail(Node* &head, Node* &tail, int val)
+void insert_at_tail(Node *&head, Node *&tail, int val)
 {
-    Node* new_node = new Node(val);
+    Node *new_node = new Node(val);
 
-    if(head == NULL)
+    if (head == NULL)
     {
         head = new_node;
         tail = new_node;
+        sz++;
         return;
     }
 
     tail->next = new_node;
     tail = new_node;
+    sz++;
 }
 
-void print_linkedList(Node* head)
+void print_linkedList(Node *head)
 {
-    Node* tmp = head;
-    while(tmp != NULL)
+    Node *tmp = head;
+    while (tmp != NULL)
     {
         cout << tmp->val << endl;
         tmp = tmp->next;
     }
 }
 
-void size_of_list(Node* head)
-{
-    int count = 0;
-    Node* tmp = head;
-    while(tmp != NULL)
-    {
-        count++;
-        tmp = tmp->next;
-    }
+// here is the naive approach to get the size of the list
 
-    cout << count << endl;
+// void size_of_list(Node* head)
+// {
+//     int count = 0;
+//     Node* tmp = head;
+//     while(tmp != NULL)
+//     {
+//         count++;
+//         tmp = tmp->next;
+//     }
+
+//     cout << count << endl;
+// }
+void size_of_list()
+{
+
+    cout << sz << endl;
 }
 
 int main()
 {
-    Node* head = NULL;
-    Node* tail = NULL;
+    Node *head = NULL;
+    Node *tail = NULL;
 
     while (1)
     {
         int x;
         cin >> x;
 
-        if(x == -1)
+        if (x == -1)
         {
             break;
         }
@@ -71,6 +82,6 @@ int main()
     }
 
     print_linkedList(head);
-    size_of_list(head);
+    size_of_list();
     return 0;
 }
