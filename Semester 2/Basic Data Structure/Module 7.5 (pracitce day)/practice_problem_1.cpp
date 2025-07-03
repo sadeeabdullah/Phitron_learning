136cbbb3beb7f07265c7e7c00933c56fa5670e92
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-07-03 07:30:43 +0600

    added problem 1 solution

--- Semester 2/Basic Data Structure/Module 7.5 (pracitce day)/practice_problem_1.cpp
+++ Semester 2/Basic Data Structure/Module 7.5 (pracitce day)/practice_problem_1.cpp
@@ -1,66 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//creating node for linked list
+// creating node for linked list
 class Node
 {
-    public:
+public:
     int val;
-    Node* next;
+    Node *next;
 
     Node(int val)
     {
         this->val = val;
-        this->next = NULL; 
+        this->next = NULL;
     }
 };
 
-//creating insert_at_tail function to insert the node in newly made list
-void insert_at_tail(Node* &head, Node* &tail, int val)
+// creating insert_at_tail1 function to insert the node in newly made list
+void insert_at_tail(Node *&head, Node *&tail1, int val)
 {
-    //creating new node with the given value that will be inserted in the linked list
-    Node* newNode = new Node(val);
-    if(head == NULL)
+    // creating new node with the given value that will be inserted in the linked list
+    Node *newNode = new Node(val);
+    if (head == NULL)
     {
         head = newNode;
-        tail = newNode;
+        tail1 = newNode;
         return; // doing return to end the execution of the function here
     }
 
-    tail->next = newNode;
-    tail = newNode;
+    tail1->next = newNode;
+    tail1 = newNode;
 }
 
-//print_list function to print the linked list
-void print_list(Node* head)
+// print_list function to print the linked list
+void print_list(Node *head)
 {
-    while(head != NULL)
+    while (head != NULL)
     {
         cout << head->val << " ";
         head = head->next;
     }
     cout << endl;
 }
+
+int get_size(Node* head)
+{
+    int sz = 0;
+    while (head->next != NULL)
+    {   sz ++;
+        head = head->next;
+    }
+    return sz;
+}
 int main()
 {
     // creating head and tail null as the linked list is blank at the beginning
-    Node* head = NULL;
-    Node* tail = NULL;
+    Node *head1 = NULL;
+    Node *tail1 = NULL;
+
+    //creating head for another linked list
+    Node *head2 = NULL;
+    Node *tail2 = NULL;
 
     // will take input untill we get -1 from the input
-    while(1)
+    while (1)
     {
         int x;
         cin >> x;
-        if(x == -1)
+        if (x == -1)
         {
             break;
         }
-        //insert at tail function to take input from the user
-        insert_at_tail(head, tail, x);
+        // insert at tail function to take input from the user
+        insert_at_tail(head1, tail1, x);
     }
 
-    //printing the list
-    print_list(head);
+
+    // will take input untill we get -1 from the input
+    while (1)
+    {
+        int x;
+        cin >> x;
+        if (x == -1)
+        {
+            break;
+        }
+        // insert at tail function to take input from the user
+        insert_at_tail(head2, tail2, x);
+    }
+
+    int sz1 = get_size(head1);
+    int sz2 = get_size(head2);
+    cout << ((sz1 == sz2) ? "YES" : "NO") << endl;
     return 0;
 }
