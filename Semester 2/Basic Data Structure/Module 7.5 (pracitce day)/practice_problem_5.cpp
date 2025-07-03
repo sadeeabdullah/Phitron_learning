fd69659dae5c5c6038dfe1f085cba0279f7b4786
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-07-03 09:11:20 +0600

    added practice problem 5 solution

---
+++ Semester 2/Basic Data Structure/Module 7.5 (pracitce day)/practice_problem_5.cpp
@@ -0,0 +1,73 @@
+// reverse print list using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+}
+
+// sort function for linked list
+void sort_list(Node *head)
+{
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->val < j->val)
+                swap(i->val, j->val);
+        }
+    }
+}
+
+// print_list function to print the linked list
+void print_list(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (1)
+    {
+        int x;
+        cin >> x;
+        if (x == -1)
+        {
+            break;
+        }
+        insert_at_tail(head, tail, x);
+    }
+
+    sort_list(head);
+
+    print_list(head);
+    return 0;
+}
