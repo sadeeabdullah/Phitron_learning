f398224faff4dd1490a188aa0c67e89c3a1675b0
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-07-12 18:10:27 +0600

    added practice 4 problem solution without STL

--- Semester 2/Basic Data Structure/Module 10.5 (practice day)/practice_4.cpp
+++ Semester 2/Basic Data Structure/Module 10.5 (practice day)/practice_4.cpp
@@ -17,6 +17,24 @@ public:
     }
 };
 
+void insert_at_head(Node *&head, Node *&tail, int val)
+{
+    Node *newnode = new Node(val);
+
+    // if (tail == NULL)
+    if (head == NULL)
+    {
+        head = newnode;
+        tail = newnode;
+        sz++;
+        return;
+    }
+    newnode->next = head;
+    head->prev = newnode;
+    head = newnode;
+    sz++;
+}
+
 void insert_at_tail(Node *&head, Node *&tail, int val)
 {
     Node *newNode = new Node(val);
@@ -25,11 +43,38 @@ void insert_at_tail(Node *&head, Node *&tail, int val)
         head = newNode;
         tail = newNode;
         sz++;
+        return;
     }
     tail->next = newNode;
     newNode->prev = tail;
     tail = newNode;
     sz++;
+}
+
+void insert_at_any_position(Node *&head, Node *&tail, int idx, int val)
+{
+    Node *newNode = new Node(val);
+
+    Node *tmp = head;
+    for (int i = 1; i < idx; i++)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next->prev = newNode;
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    newNode->prev = tmp;
+    sz++;
+}
+
+void print_forward(Node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
 }
 
 void print_backward(Node *tail)
@@ -41,52 +86,39 @@ void print_backward(Node *tail)
     }
     cout << endl;
 }
-void print_forward(Node *head)
-{
-    while (head != NULL)
-    {
-        cout << head->val << " ";
-        head = head->next;
-    }
-    cout << endl;
-}
 
-void insert_at_any_position(Node *head, Node *tail, int idx, int val)
-{
-    Node *newNode = new Node(val);
-    if (idx == 0)
-    {
-        head == newNode;
-        tail = newNode;
-        sz++;
-        return;
-    }
-    for (int i = 1; i < idx; i++)
-    {
-        head = head->next;
-    }
-    head->next->prev = newNode;
-    newNode->next = head->next;
-    head->next = newNode;
-    newNode->prev = head;
-    sz++;
-}
 int main()
 {
     Node *head = NULL;
     Node *tail = NULL;
-    int x, y;
-    cin >> x >> y;
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int idx, val;
+        cin >> idx >> val;
 
-    if (x > sz)
-    {
-        cout << "Invalid" << endl;
+        if (idx < 0 || idx > sz)
+        {
+            cout << "Invalid" << endl;
+            continue;
+        }
+        else if (idx == sz)
+        {
+            insert_at_tail(head, tail, val);
+        }
+        else if (idx == 0)
+        {
+            insert_at_head(head, tail, val);
+        }
+        else
+        {
+            insert_at_any_position(head, tail, idx, val);
+        }
+
+        print_forward(head);
+        print_backward(tail);
     }
-    else
-    {
-        insert_at_any_position(head, tail, x, y);
-    }
-    print_forward(head);
-    print_backward(tail);
+
     return 0;
 }
