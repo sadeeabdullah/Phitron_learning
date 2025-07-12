538180be363e5c49367fe87070a2699da28dff29
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-07-12 18:20:35 +0600

    added practice 4 problem solution with stl

---
+++ Semester 2/Basic Data Structure/Module 10.5 (practice day)/practice_4_solution_with_stl.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sz = 0;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(list<int> &l)
+{
+    for (int val : l)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+}
+
+void print_backward(list<int> &l)
+{
+    // copying this list and reversing
+    list<int> x = l;
+    reverse(x.begin(), x.end());
+    for (int val : x)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    list<int> l;
+    int q;
+    cin >> q;
+    while (q--)
+    {
+        int idx, val;
+        cin >> idx >> val;
+
+        if (idx < 0 || idx > sz)
+        {
+            cout << "Invalid" << endl;
+            continue;
+        }
+        else if (idx == sz)
+        {
+            l.push_back(val);
+            sz++;
+        }
+        else if (idx == 0)
+        {
+            l.push_front(val);
+            sz++;
+        }
+        else
+        {
+            l.insert(next(l.begin(), idx), val);
+            sz++;
+        }
+
+        print_forward(l);
+        print_backward(l);
+    }
+
+    return 0;
+}
