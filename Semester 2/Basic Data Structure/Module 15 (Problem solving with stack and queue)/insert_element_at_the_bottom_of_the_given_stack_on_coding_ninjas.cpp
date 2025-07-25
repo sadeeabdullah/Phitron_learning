7c98533e5fdfc27e35ed4b5c8f54f869c805d780
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-07-25 14:57:45 +0600

    added how to insert element add the bottom of a stack problem  solution on coding ninjas.

---
+++ Semester 2/Basic Data Structure/Module 15 (Problem solving with stack and queue)/insert_element_at_the_bottom_of_the_given_stack_on_coding_ninjas.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+stack<int> pushAtBottom(stack<int>& myStack, int x) 
+{
+    //creating new stack for copying and storing the value of the given stack
+    stack<int> newStack;
+    while(!myStack.empty())
+    {
+        newStack.push(myStack.top());
+        myStack.pop();
+    }
+
+    //after completing pushing the value of the given stack to new stack our given stack is fully blank now we will 
+    //add new value given by user in given stack
+    myStack.push(x);
+    
+    //again adding all the value from to the given stack so that the insertion procedure become fulfill.
+    while(!newStack.empty())
+    {
+        myStack.push(newStack.top());
+        newStack.pop();
+    }
+    return myStack;
+}
