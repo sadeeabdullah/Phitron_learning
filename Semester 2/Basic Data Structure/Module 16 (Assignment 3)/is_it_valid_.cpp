3fdcf0dc19abde7958262eac5422fd3849e1b90c
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-07-26 18:47:30 +0600

    added basic structure for the problem solution

---
+++ Semester 2/Basic Data Structure/Module 16 (Assignment 3)/is_it_valid_.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<char> st;
+    int tc; cin >> tc;
+    while (tc--)
+    {
+        string s;
+        cin >> s;
+        for (char c : s)
+        {
+            
+        }
+
+    }
+    return 0;
+}
