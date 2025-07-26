6cf399795c0947d87e6be15f601c70ff9763a40e
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-07-26 19:38:05 +0600

    added is it valid problem solution

--- Semester 2/Basic Data Structure/Module 16 (Assignment 3)/is_it_valid_.cpp
+++ Semester 2/Basic Data Structure/Module 16 (Assignment 3)/is_it_valid_.cpp
@@ -2,16 +2,26 @@
 using namespace std;
 int main()
 {
-    stack<char> st;
     int tc; cin >> tc;
     while (tc--)
     {
+        stack<char> st;
         string s;
         cin >> s;
         for (char c : s)
         {
-            
+            if (!st.empty() && st.top() == '1' && c == '0')
+                st.pop();
+            else if (!st.empty() && st.top() == '0' && c == '1')
+            {
+                st.pop();
+            }
+            else 
+            {
+                st.push(c);
+            }
         }
+        cout << ((st.empty()) ? "YES" : "NO") << endl;
 
     }
     return 0;
