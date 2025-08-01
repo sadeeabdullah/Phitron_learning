2c151952755f938b3cb516b9bd5265ddfd58193b
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-01 12:53:11 +0600

    added how to  make vector with pair.

--- Semester 2/Basic Data Structure/Module 19 (problem solving with binary tree)/pair.cpp
+++ Semester 2/Basic Data Structure/Module 19 (problem solving with binary tree)/pair.cpp
@@ -2,9 +2,24 @@
 using namespace std;
 int main()
 {
-    pair<string, int> p;
-    //p = make_pair("sadee", 100);
-    p = {"sadee", 100};
-    cout << p.first << " " << p.second << endl;
+    // pair<string, int> p;
+    // //p = make_pair("sadee", 100);
+    // p = {"sadee", 100};
+    // cout << p.first << " " << p.second << endl;
+
+    //making a vector pair
+    int n; 
+    cin >> n;
+    vector<pair<int, int> > v(n);
+
+    //taking input of the pair
+    for (int i = 0; i < n ; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+    for (int i = 0; i < n ; i++)
+    {
+        cout << v[i].first << " " <<  v[i].second << endl;
+    }
     return 0;
 }
