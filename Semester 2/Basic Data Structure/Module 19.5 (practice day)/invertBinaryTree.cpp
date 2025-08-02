b090b397109998aa2a67ce6cbc241dabea3210c8
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-02 07:12:26 +0600

    added binary tree polished solution

--- Semester 2/Basic Data Structure/Module 19.5 (practice day)/invertBinaryTree.cpp
+++ Semester 2/Basic Data Structure/Module 19.5 (practice day)/invertBinaryTree.cpp
@@ -13,40 +13,12 @@
  */
 class Solution {
 public:
-// creating vector to return
-    vector<int> v;
-    // using freq arr
-    bool freq[105] = {false};
-    vector<int> rightSideView(TreeNode* root) {
-// creating function to get the right view traversing whole tree in level order
-
-
-    //taking pair for tracking the level
-    pair<TreeNode *, int> pr;
-    //taking a queue for traversing
-    queue<pair<TreeNode *, int>> q;
-//manually pushing the root value into it
-    if (root)
-        q.push({root,1});
-    while (!q.empty())
-    {
-        pair<TreeNode *, int> p = q.front();
-        q.pop();
-
-        //spliting the pair
-        TreeNode *node = p.first;
-        int level = p.second;
-        if (freq[level] == false)
-        {
-            v.push_back(node->val);
-            freq[level] = true;
-        }
-        if (node->right)
-            q.push({node->right, level + 1});
-        if (node->left)
-            q.push({node->left, level + 1});
-    }
-    return v;
+    TreeNode* invertTree(TreeNode* root) {
         
+        if (root == NULL) return root;
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
     }
 };
