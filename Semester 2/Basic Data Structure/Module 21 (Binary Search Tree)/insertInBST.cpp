6fc8c9953e7e472dd2ad0055e106ac813a6662d5
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-04 07:42:08 +0600

    added inserting node in BST code

---
+++ Semester 2/Basic Data Structure/Module 21 (Binary Search Tree)/insertInBST.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *inputTree()
+{
+    queue<Node *> q;
+
+    Node *root;
+    int val;
+    cin >> val;
+    if (val == -1)
+    {
+        root = NULL;
+    }
+    else
+    {
+        root = new Node(val);
+    }
+
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node *myLeft, *myRight;
+
+        if (l == -1)
+            myLeft = NULL;
+        else
+            myLeft = new Node(l);
+        if (r == -1)
+            myRight = NULL;
+        else
+            myRight = new Node(r);
+
+        // connect the child with the root
+        p->left = myLeft;
+        p->right = myRight;
+
+        if (myLeft)
+            q.push(myLeft);
+        if (myRight)
+            q.push(myRight);
+    }
+    return root;
+}
+
+void levelOrder(Node *root)
+{
+    queue<Node *> q;
+
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        cout << p->val << " ";
+
+        if (p->left)
+            q.push(p->left);
+        if (p->right)
+            q.push(p->right);
+    }
+}
+
+void insert(Node *&root, int val)
+{
+    // here we are changing the root so we need to use refference
+    if (root == NULL)
+        root = new Node(val);
+    if (root->val > val)
+    {
+        if (root ->left == NULL)
+        root->left = new Node(val);
+    else
+        insert(root->left, val);
+    }
+    else
+    {
+        if(root->right == NULL)
+            root->right = new Node(val);
+        else
+            insert(root->right, val);
+    }
+}
+int main()
+{
+    Node *root = inputTree();
+
+    int val;
+    cin >> val;
+    insert(root, val);
+    levelOrder(root);
+    return 0;
+}
