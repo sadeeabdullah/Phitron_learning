d11ba2b37060be032f90e0e143049e3d43a1e6d5
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-04 20:41:40 +0600

    added convert sort array to BST problem solution

---
+++ Semester 2/Basic Data Structure/Module 21 (Binary Search Tree)/Practice Problem/convertSortArrayToBST.cpp
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/submissions/1723173004/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode *convert(vector<int> num, int l, int r)
+    {
+        if (l > r)
+            return NULL;
+        int mid = (l + r) / 2;
+
+        TreeNode *root = new TreeNode(num[mid]);
+        TreeNode *leftRoot = convert(num, l, mid - 1);
+        TreeNode *rightRoot = convert(num, mid + 1, r);
+        root->left = leftRoot;
+        root->right = rightRoot;
+        return root;
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        TreeNode *root = convert(nums, 0, nums.size() - 1);
+        return root;
+    }
+};
