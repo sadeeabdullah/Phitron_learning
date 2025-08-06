9da1e7f1886bad3b83550009855b5a41adecd80e
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-06 17:43:10 +0600

    added most optimized solution

--- Semester 2/Basic Data Structure/Module 22.5 (Practice Day)/maximumProductOfTwoElementInAnArray.cpp
+++ Semester 2/Basic Data Structure/Module 22.5 (Practice Day)/maximumProductOfTwoElementInAnArray.cpp
@@ -35,3 +35,27 @@ public:
         return maxResult;
     }
 };
+
+
+// best optimize Solution:  time complexity O(N)
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int mx1 = INT_MIN, mx2 = INT_MIN;
+
+        for (int x  : nums)
+        {
+            if ( x >= mx1)
+            {
+                mx2 = mx1;
+                mx1 = x;
+            }
+            else if (x <= mx1 && x >= mx2)
+            {
+                mx2 = x;
+            }
+        }
+
+        return (mx1 - 1) * (mx2 - 1);
+    }
+};
