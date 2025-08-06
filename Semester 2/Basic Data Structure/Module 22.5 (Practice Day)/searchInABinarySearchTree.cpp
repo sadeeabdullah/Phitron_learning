188280c79dc1ea3b01222e5dbaa6c95cafbce3fb
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-06 16:23:43 +0600

    added solution with recursion

--- Semester 2/Basic Data Structure/Module 22.5 (Practice Day)/searchInABinarySearchTree.cpp
+++ Semester 2/Basic Data Structure/Module 22.5 (Practice Day)/searchInABinarySearchTree.cpp
@@ -23,3 +23,23 @@ public:
         return NULL;
     }
 };
+
+
+
+// solution with recursion
+
+class Solution {
+public:
+
+    
+    TreeNode* searchBST(TreeNode* root, int val) {
+    //with recursion
+    if (!root) return NULL;
+    if (root ->val == val)
+        return root;
+    if (val < root->val)
+        return searchBST(root->left, val);
+    else
+        return searchBST(root->right, val);
+    }
+};
