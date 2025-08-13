22f16da995ac15418d2cec0e7c1baf514398a4c1
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-13 22:30:25 +0600

    added sample comment

--- Semester 2/Basic Data Structure/XCPC prep practice problem/A_Phone_Desktop.cpp
+++ Semester 2/Basic Data Structure/XCPC prep practice problem/A_Phone_Desktop.cpp
@@ -28,7 +28,7 @@ int main()
             {
                 // we need another screen
                 screen++;
-            }
+            } // sample comment
         }
 
         cout << screen << endl;
