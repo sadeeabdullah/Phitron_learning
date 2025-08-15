8a991958469c5b521782ead5325ebbb1ccccbbc9
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-15 21:15:41 +0600

    added basic solution of contest

---
+++ Semester 2/Basic Data Structure/phitron codefest 03 Batch 07/findIt.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// creating nodes for binary tree
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL; 
+        this->right = NULL; 
+    }
+};
+
+//creating function for taking input value of nodes in a tree
+inputTree()
+{
+    // taking queue
+    queue<Node *> q;
+}
+int main()
+{
+    // for input the tree
+    inputTree();
+    return 0;
+}
