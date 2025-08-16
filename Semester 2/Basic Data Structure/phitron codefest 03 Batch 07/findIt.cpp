a614b183f2044aa87145d8685034d6f5d0a318c7
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-16 15:21:34 +0600

    added input function in level order

--- Semester 2/Basic Data Structure/phitron codefest 03 Batch 07/findIt.cpp
+++ Semester 2/Basic Data Structure/phitron codefest 03 Batch 07/findIt.cpp
@@ -12,20 +12,68 @@ public:
     Node(int val)
     {
         this->val = val;
-        this->left = NULL; 
-        this->right = NULL; 
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
-//creating function for taking input value of nodes in a tree
-inputTree()
+// creating function for taking input value of nodes in a tree
+Node *inputTree()
 {
+    int n;
+    cin >> n;
+    Node *root;
+
+    if (n != -1)
+        root = new Node(n);
+    else
+        root = NULL;
+
     // taking queue
     queue<Node *> q;
+
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int left, right;
+        Node *leftVal, *rightVal;
+        cin >> left >> right;
+
+        if (left == -1)
+            leftVal = NULL;
+        else
+            leftVal = new Node(left);
+        if (right == -1)
+            rightVal = NULL;
+        else
+            rightVal = new Node(right);
+
+        // connecting left and right value to the root node
+
+        p->left = leftVal;
+        p->right = rightVal;
+
+        // now pushing these value to the queue
+        if (leftVal)
+            q.push(leftVal);
+        if (rightVal)
+            q.push(rightVal);
+    }
+
+    return root;
 }
+
+
+
 int main()
 {
     // for input the tree
-    inputTree();
+    Node *root = inputTree();
+    levelOrder(root);
     return 0;
 }
