d6e6f8bced5fa2be9d734c2cbd9ebe2d11923184
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-29 23:50:24 +0600

    added minimum knight moves problem solution

---
+++ Semester 2/Algorithm/Conceptual session/Week 2 session 2/Minimum_Knight_moves.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char grid[8][8];
+bool vis[8][8];
+int level[8][8];
+
+vector<pair<int, int>> d = {{2, -1}, {1, -2}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}};
+
+int n = 8, m = 8;
+bool valid (int i, int j)
+{
+    return (i >= 0 && i < n && j >= 0 && j < m);
+}
+
+void bfs(int si, int sj)
+{
+    queue<pair<int, int>> q;
+    q.push({si,sj});
+    vis[si][sj] = true;
+    level[si][sj] = 0;
+
+    while(!q.empty())
+    {
+        auto par = q.front();
+        q.pop();
+
+        // splitting
+        int par_i = par.first;
+        int par_j = par.second;
+
+        for (auto dir : d)
+        {
+            int ci  = par_i + dir.first;
+            int cj = par_j + dir.second;
+            if (valid(ci, cj) && !vis[ci][cj])
+            {
+                q.push({ci, cj});
+                vis[ci][cj] = true;
+                level[ci][cj] = level[par_i][par_j] + 1;
+            }
+        }
+    }
+}
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    // we are getting a index in matrix in character we have to convert it to real index
+    int si = a[0] - 'a';
+    int sj = a[1] - '1';
+    int di = b[0] - 'a';
+    int dj = b[1] - '1';
+
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+    
+    bfs(si, sj);
+    
+    cout << level[di][dj];
+    return 0;
+}
