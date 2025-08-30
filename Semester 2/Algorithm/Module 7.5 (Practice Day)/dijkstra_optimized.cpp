159f3f18cf317db91469cb2cab04ef686a3e821a
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-08-30 15:30:51 +0600

    added dijkstra time complexity

--- Semester 2/Algorithm/Module 7.5 (Practice Day)/dijkstra_optimized.cpp
+++ Semester 2/Algorithm/Module 7.5 (Practice Day)/dijkstra_optimized.cpp
@@ -3,23 +3,24 @@ using namespace std;
 vector<pair<int, int>> adj_list[1005];
 vector<int> dis(1005, INT_MAX);
 
-void dijkstra(int src)
+void dijkstra(int src) // tc : O(v log v  + E log v) or Log V ( V + E)
 {
-    queue<pair<int, int>> q;
-    q.push({src, 0});
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; // tc for push and pop O(log V)
+
+    pq.push({0, src}); // giving the dis first as we know pair in priority queue sort by the first value
 
     dis[src] = 0;
 
-    while (!q.empty())
+    while (!pq.empty()) // tc : O(v)
     {
-        auto par = q.front();
-        q.pop();
+        auto par = pq.top();
+        pq.pop(); // tc : O(v log v)
 
         // splitting
-        int par_node = par.first;
-        int par_dis = par.second;
+        int par_node = par.second;
+        int par_dis = par.first;
 
-        for (auto child : adj_list[par_node])
+        for (auto child : adj_list[par_node]) // tc : O(E)
         {
             // splitting child
             int child_node = child.first;
@@ -28,7 +29,7 @@ void dijkstra(int src)
             if (par_dis + child_dis < dis[child_node])
             {
                 dis[child_node] = par_dis + child_dis;
-                q.push({child_node, dis[child_node]});
+                pq.push({dis[child_node], child_node}); // tc : (E log V)
             }
         }
     }
