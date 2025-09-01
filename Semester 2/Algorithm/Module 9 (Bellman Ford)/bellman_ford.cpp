816106879f810fa28f4e734ad0c57c4b5897d6a4
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-09-01 06:05:09 +0600

    added bellman ford algo negative and cycle detection

--- Semester 2/Algorithm/Module 9 (Bellman Ford)/bellman_ford.cpp
+++ Semester 2/Algorithm/Module 9 (Bellman Ford)/bellman_ford.cpp
@@ -35,6 +35,29 @@ void bellman_ford()
                 dis[b] = dis[a] + c;
             }
         }
+
+    bool cycle = false;
+    for (auto ed : edge_list)
+    {
+        int a, b, c;
+        a = ed.a;
+        b = ed.b;
+        c = ed.c;
+
+        if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+        {
+            cycle = true;
+            break;
+        }
+    }
+
+    if (cycle)
+        cout << "Negative cycle detected" << endl;
+    else
+        for (int i = 0; i < n; i++)
+        {
+            cout << i << " -> " << dis[i] << endl;
+        }
 }
 int main()
 {
@@ -55,9 +78,5 @@ int main()
 
     bellman_ford();
 
-    for (int i = 0; i < n; i++)
-    {
-        cout << i << " -> " << dis[i] << endl;
-    }
     return 0;
 }
