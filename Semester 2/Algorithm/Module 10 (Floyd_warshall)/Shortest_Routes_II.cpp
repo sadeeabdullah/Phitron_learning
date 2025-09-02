9575fbc2820957ef30e1b6a23884eb468c80cd2d
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-09-02 22:16:13 +0600

    added

---
+++ Semester 2/Algorithm/Module 10 (Floyd_warshall)/Shortest_Routes_II.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// we will use class for storing the a, b and c;
+class sh
+{
+public:
+    int a, b, c;
+
+};
+ vector<pair<int,int>> adj_list[505];
+
+int main()
+{
+    int n, m, q;
+
+    // creating pair of pari
+    while (m--)
+    {
+        int a, b,c;
+        cin >> a >> b >> c;
+        sh.a = a;
+        sh.b = b;
+        sh.c = c;
+
+        adj_list[a].push_back()
+    }
+    return 0;
+}
