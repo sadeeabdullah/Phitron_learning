9411aba995cdf36ddd889046eb8458d88baa3a1c
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-09-12 01:12:03 +0600

    added maximum path sum problem solution with dp

--- Semester 2/Algorithm/Module 15.5 (Practice day)/X_The_maximum_path_sum.cpp
+++ Semester 2/Algorithm/Module 15.5 (Practice day)/X_The_maximum_path_sum.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int n, m;
 int mat[15][15];
+int dp[15][15]; // memoization table here
 
 int getMax(int i, int j)
 {
@@ -9,12 +10,16 @@ int getMax(int i, int j)
     if (i == 0 && j == 0)
         return mat[i][j];
     
+    // if the max value is stored
+    if (dp[i][j] != -1)
+        return dp[i][j];
     int up = INT_MIN, left = INT_MIN;
     if (i > 0)  
         up = getMax(i - 1, j);
     if (j > 0)
         left = getMax(i, j - 1);
-    return mat[i][j] + max(up, left);
+    dp[i][j] =  mat[i][j] + max(up, left);
+    return dp[i][j];
 }
 int main()
 {
@@ -23,6 +28,8 @@ int main()
     for (int i =0; i < n; i++)
         for (int j = 0; j < m; j++)
             cin >> mat[i][j];
+
+    memset(dp, -1, sizeof(dp));
     // calling the recursive funciton;
     cout << getMax(n - 1, m - 1) << endl;
     return 0;
