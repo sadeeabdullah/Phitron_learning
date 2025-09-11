c30e2faea52a4ee4dcc8a3b3d21500d9cdb37e00
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-09-11 23:35:07 +0600

    added knapsack problem without dp

---
+++ Semester 2/Algorithm/Module 15.5 (Practice day)/U_Knapsack.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, w;
+int weight[55], val[1005];
+
+int knapsack(int n, int mx_weight)
+{
+    if (n < 0)
+        return 0;
+    
+    if (weight[n] <= mx_weight)
+    {
+        // will take in bag
+        int op1 = knapsack(n - 1, mx_weight - weight[n]) + val[n];
+        int op2 = knapsack(n - 1, mx_weight);
+
+        return max(op1, op2);
+    }
+    else
+        return knapsack(n - 1, mx_weight);
+}
+int main()
+{
+    cin >> n >> w;
+
+    // taking input the both array
+    for (int i = 0; i < n; i++)
+        cin >> weight[i] >> val[i];
+        
+    // we will call the 0/1 knapsack function here
+    cout << knapsack(n - 1, w) << endl;
+    return 0;
+}
