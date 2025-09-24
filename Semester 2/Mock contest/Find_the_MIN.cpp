086076217a6784eb11b8b746769ce2cf5b7df915
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-09-24 22:52:35 +0600

    added approach but got error

---
+++ Semester 2/Mock contest/Find_the_MIN.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class cmp
+{
+public:
+    bool operator()(int a, int b)
+    {
+        return a > b;
+    }
+};
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    priority_queue<int, vector<int>, cmp> pq;
+
+    int query;
+    cin >> query;
+    while (query--)
+    {
+        int i;
+        cin >> i;
+        if (i == 1)
+        {
+            int x;
+            cin >> x;
+            pq.push(x);
+        }
+        else
+        {
+            if (pq.empty())
+            {
+                cout << "empty" << '\n';
+            }
+            else
+            {
+
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }
+    }
+    return 0;
+}
