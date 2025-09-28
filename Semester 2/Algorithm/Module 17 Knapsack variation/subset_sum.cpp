96315102da603be4676a15ed1b8561980c4538f9
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-09-28 20:10:09 +0600

    added dp

--- Semester 2/Algorithm/Module 17 Knapsack variation/subset_sum.cpp
+++ Semester 2/Algorithm/Module 17 Knapsack variation/subset_sum.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 int val[1005];
 
+int dp[1005][1005];
+
 // creating the subset funtion
 bool subset_sum(int i, int sum)
 {
@@ -22,17 +24,23 @@ bool subset_sum(int i, int sum)
             return false;
         }
     }
-
+    
+    if (dp[i][sum] != -1)
+    {
+        return dp[i][sum];
+    }
     if (val[i] <= sum)
     {
 
         bool op1 = subset_sum(i - 1, sum - val[i]);
         bool op2 = subset_sum(i - 1, sum);
-        return op1 || op2;
+        dp[i][sum] =  op1 || op2;
+        return dp[i][sum];
     }
     else
     {
-        return subset_sum(i - 1, sum);
+        dp[i][sum] =  subset_sum(i - 1, sum);
+        return dp[i][sum];
     }
 }
 int main()
@@ -46,6 +54,10 @@ int main()
 
     int sum;
     cin >> sum;
+
+    // setting dp array to false
+    memset(dp, -1, sizeof(dp));
+
     bool isPossible = subset_sum(n - 1, sum);
 
     cout << ((isPossible) ?  "YES" : "NO") << nl;
