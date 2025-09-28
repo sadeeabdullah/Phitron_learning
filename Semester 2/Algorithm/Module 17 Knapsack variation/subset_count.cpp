89ff6c997888e0691640d62b30c38a7a5b8b674c
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-09-28 22:38:11 +0600

    added subset of sum possible way count

--- Semester 2/Algorithm/Module 17 Knapsack variation/subset_count.cpp
+++ Semester 2/Algorithm/Module 17 Knapsack variation/subset_count.cpp
@@ -6,43 +6,45 @@
     cin.tie(NULL);
 using namespace std;
 int val[1005];
-
 int dp[1005][1005];
 
-// creating the subset funtion
-bool subset_sum(int i, int sum) // TC: O(n * s)
+int sum_count(int i, int sum)
 {
+
+    //base case here
     if (i < 0)
     {
         if (sum == 0)
         {
-
-            return true;
+            return 1;
         }
         else
         {
-            return false;
+            return 0;
         }
     }
-    
-    if (dp[i][sum] != -1)
+
+    // if the result is already storeed then return from there
+    if(dp[i][sum] != -1)
     {
         return dp[i][sum];
     }
     if (val[i] <= sum)
     {
 
-        bool op1 = subset_sum(i - 1, sum - val[i]);
-        bool op2 = subset_sum(i - 1, sum);
-        dp[i][sum] =  op1 || op2;
+        int op1 = sum_count(i - 1, sum - val[i]);
+        int op2 = sum_count(i - 1, sum);
+
+        dp[i][sum] = op1 + op2;
         return dp[i][sum];
     }
     else
     {
-        dp[i][sum] =  subset_sum(i - 1, sum);
+        dp[i][sum] =  sum_count(i - 1, sum);
         return dp[i][sum];
     }
 }
+
 int main()
 {
     fastIO();
@@ -50,16 +52,17 @@ int main()
     int n;
     cin >> n;
     for (int i = 0; i < n; i++)
+    {
         cin >> val[i];
+    }
 
     int sum;
     cin >> sum;
-
-    // setting dp array to false
     memset(dp, -1, sizeof(dp));
 
-    bool isPossible = subset_sum(n - 1, sum);
 
-    cout << ((isPossible) ?  "YES" : "NO") << nl;
+
+    int total_way_of_sum = sum_count(n - 1, sum);
+    cout << total_way_of_sum << nl;
     return 0;
 }
--- Semester 2/Algorithm/Module 17 Knapsack variation/subset_sum_equal.cpp
+++ Semester 2/Algorithm/Module 17 Knapsack variation/subset_sum_equal.cpp
@@ -6,17 +6,16 @@
     cin.tie(NULL);
 using namespace std;
 int val[1005];
-
 int dp[1005][1005];
 
-// creating the subset funtion
-bool subset_sum(int i, int sum) // TC: O(n * s)
+bool sum_equal(int i, int sum)
 {
+
+    //base case here
     if (i < 0)
     {
         if (sum == 0)
         {
-
             return true;
         }
         else
@@ -24,42 +23,50 @@ bool subset_sum(int i, int sum) // TC: O(n * s)
             return false;
         }
     }
-    
-    if (dp[i][sum] != -1)
+
+    // if the result is already storeed then return from there
+    if(dp[i][sum] != -1)
     {
         return dp[i][sum];
     }
     if (val[i] <= sum)
     {
 
-        bool op1 = subset_sum(i - 1, sum - val[i]);
-        bool op2 = subset_sum(i - 1, sum);
-        dp[i][sum] =  op1 || op2;
+        bool op1 = sum_equal(i - 1, sum - val[i]);
+        bool op2 = sum_equal(i - 1, sum);
+
+        dp[i][sum] = op1 | op2;
         return dp[i][sum];
     }
     else
     {
-        dp[i][sum] =  subset_sum(i - 1, sum);
+        dp[i][sum] =  sum_equal(i - 1, sum);
         return dp[i][sum];
     }
 }
+
 int main()
 {
     fastIO();
 
     int n;
     cin >> n;
+    int total = 0;
     for (int i = 0; i < n; i++)
+    {
         cin >> val[i];
+        total += val[i];
+    }
 
-    int sum;
-    cin >> sum;
-
-    // setting dp array to false
+    if (total % 2 != 0)
+    {
+        cout << "No" << nl;
+        return 0;
+    }
     memset(dp, -1, sizeof(dp));
+    int sum = total / 2;
 
-    bool isPossible = subset_sum(n - 1, sum);
-
-    cout << ((isPossible) ?  "YES" : "NO") << nl;
+    bool isPossible = sum_equal(n - 1, sum);
+    cout << ((isPossible) ? "yes" : "no") << nl;
     return 0;
 }
