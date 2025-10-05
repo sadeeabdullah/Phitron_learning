4e1cd8092bb99f7d9bfed288e80370d55fbdff73
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-10-05 22:33:37 +0600

    added this problem solution

---
+++ Cp Track/Newbie/Week 1/A_Approximate_Answer.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main ()
+{
+    fastIO();
+    
+    int x, y, k;
+    cin >> x >> y >> k;
+    cout << (!(abs(x - y) <= k) ? "No" : "Yes") << nl;
+    return 0;
+}
