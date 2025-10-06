5553a0cb2ad473ff8b989f223751474f00070763
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-10-06 18:44:31 +0600

    added draw a square problem solution

---
+++ Cp Track/Newbie/Week 1/A_Draw_a_Square.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main ()
+{
+    fastIO();
+    
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        int l, r, d, c;
+        cin >> l >> r >> d >> c;
+        cout << ((l == r && r == d && d == c) ? "Yes" : "No") << nl;
+    }
+    return 0;
+}
