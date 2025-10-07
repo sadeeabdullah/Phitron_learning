daa088095312d7647df1d2ef9ef3f69e95394f52
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-10-07 22:23:51 +0600

    added carrots tab metadata problem solution

---
+++ Cp Track/Newbie/Week 1/carrots_tab_metadata.cpp
@@ -0,0 +1,23 @@
+// problem link : https://open.kattis.com/problems/carrots
+
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main ()
+{
+    fastIO();
+    int n, s;
+    cin >> n >> s;
+    while(n--)
+    {
+        string str;
+        cin >> str;
+    }   
+
+    cout << s << nl;
+    return 0;
+}
