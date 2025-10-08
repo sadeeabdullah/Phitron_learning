9a5321480a3a62166de9611281f3bc64cbeb49c2
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-10-08 20:33:22 +0600

    added gambling problem solution

---
+++ Cp Track/Newbie/Week 1/Codechef contest/Gambling.cpp
@@ -0,0 +1,16 @@
+// problem link : https://www.codechef.com/START207D/problems/GAMBLING
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main ()
+{
+    fastIO();
+ 
+    int n;
+    cin >> n;
+    cout << 7 - n << nl;
+    return 0;
+}
