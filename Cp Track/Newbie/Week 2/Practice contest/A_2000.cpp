b802c76b967ab38e5ad271494e69e8900288bb57
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-10-16 20:38:48 +0600

    added 2000 problem solution

---
+++ Cp Track/Newbie/Week 2/Practice contest/A_2000.cpp
@@ -0,0 +1,16 @@
+// problem linnk : https://vjudge.net/contest/757589#problem/A
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main ()
+{
+    fastIO();
+    int n;
+    cin >> n;
+    cout << (n *2 * 2) << nl;
+    return 0;
+}
