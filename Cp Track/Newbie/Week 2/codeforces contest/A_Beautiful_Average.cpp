e7a02580db8cc9d95a507b101b0c6fe4802befe5
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-10-17 21:10:15 +0600

    added beautiful average problem solution

---
+++ Cp Track/Newbie/Week 2/codeforces contest/A_Beautiful_Average.cpp
@@ -0,0 +1,27 @@
+//problem link : https://codeforces.com/contest/2162/problem/0
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main ()
+{
+    fastIO();
+ 
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+
+        cout << *max_element(arr.begin(), arr.end()) << endl;
+
+    }
+    return 0;
+}
---
+++ Cp Track/Newbie/Week 2/codeforces contest/B_Beautiful_String.cpp
@@ -0,0 +1,14 @@
+// problem link : https://codeforces.com/contest/2162/problem/B
+
+#include <bits/stdc++.h>
+#define ll long long int
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+
+int main ()
+{
+    fastIO();
+    
+    return 0;
+}
