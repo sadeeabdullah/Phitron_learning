4e2f45e1cca675f9aeee1c7c9338db7586cb6320
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-11-26 23:32:40 +0600

    added contest code

---
+++ Cp Track/Newbie/Week 7/code/Chef_and_Close_Friends.cpp
@@ -0,0 +1,49 @@
+//Bismillah hir rahmanir rahim
+//keep Patience, Destiny is more beautiful
+
+
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout << "YES"<< nl;
+#define no cout << "NO" << nl;
+#define loop(start, end,var) for (int var = (start); var <= (end); var++)
+#define rsORT(var) sort(var.rbegin(), var.rend())
+#define sORT(var) sort(var.begin(), var.end())
+#define tInt(var) int var; cin >> var;
+#define tLL(var)  ll var;  cin >> var;
+#define tStr(var) string var; cin >> var;
+
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T> using pbds = tree <T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+   int x, y, z;
+   cin >> x >> y >> z;
+int low  = max (x - y, x - z);
+int high = min (x + y, x + z);
+cout << max(0, high - low) << nl;
+}
+int main ()
+{
+    fastIO();
+    
+    int t  = 1;
+    cin >> t;
+    while (t--)
+    {
+       solve();
+    }
+    return 0;
+}
