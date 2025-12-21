621d17d4dc8f1ca642e0e11a505b8fec89ecd77c
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-12-21 22:13:43 +0600

    added some problem solution  on the speed contest

---
+++ Cp Track/Newbie/Speed contest/contest 7/D_Add_to_get_GCD.cpp
@@ -0,0 +1,64 @@
+// Bismillah hir rahmanir rahim
+// keep Patience, Destiny is more beautiful
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define fastIO()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define yes cout << "YES" << nl;
+#define no cout << "NO" << nl;
+#define loop(start, end, var) for (int var = (start); var <= (end); var++)
+#define rsORT(var) sort(var.rbegin(), var.rend())
+#define sORT(var) sort(var.begin(), var.end())
+#define tStr(var) \
+    string var;   \
+    cin >> var;
+#define testCase \
+    int t;       \
+    cin >> t;    \
+    while (t--)
+
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    int x, y;
+    cin >> x >> y;
+    if (__gcd(x, y) > 1)
+    {
+        cout << 0 << nl;
+        return;
+    }
+
+    if (__gcd(x + 1, y) > 1 || __gcd(x, y + 1) > 1)
+    {
+
+        cout << 1 << nl;
+    }
+    else
+    {
+        cout << 2 << nl;
+    }
+}
+int main()
+{
+    fastIO();
+    testCase
+    {
+
+        solve();
+    }
+    return 0;
+}
