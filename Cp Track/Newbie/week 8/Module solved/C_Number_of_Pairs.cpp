748a74278f5fee46e0298ab16256715346eac263
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-12-22 11:39:14 +0600

    added brute force solution

---
+++ Cp Track/Newbie/week 8/Module solved/C_Number_of_Pairs.cpp
@@ -0,0 +1,48 @@
+// problem link : https://codeforces.com/problemset/problem/1538/C
+//Bismillah hir rahmanir rahim
+//keep Patience, Destiny is more beautiful
+
+
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout << "YES"<< nl;
+#define no cout << "NO" << nl;
+#define loop(start, end,var) for (int var = (start); var <= (end); var++)
+#define rsORT(var) sort(var.rbegin(), var.rend())
+#define sORT(var) sort(var.begin(), var.end())
+#define tStr(var) string var; cin >> var;
+#define testCase int t; cin >> t; while(t--)
+
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T> using pbds = tree <T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+   int n, l, r, ans = 0;     cin >> n >> l >> r;
+   vector<int> a(n);    for(int i= 0; i <n ; i++)   cin >>a[i];
+
+   for(int i = 0; i < n - 1; i++)   for(int j = i + 1; j < n ; j++){
+        if(a[i] + a[j] >= l && a[i] + a[j] <= r)    ans++;
+   }
+   cout << ans << nl;
+}
+int main ()
+{
+    fastIO();
+    testCase{
+
+        solve();
+    }
+    return 0;
+}
