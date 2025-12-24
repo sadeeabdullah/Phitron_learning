ebbafc5cb020512d3be9559b71b0d3fe1af8fd9c
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-12-24 20:26:49 +0600

    added one more code in revision of div 4 217

---
+++ Cp Track/Newbie/codechef contest/codechef upsolve/div 4 217/Add_to_make_Positive.cpp
@@ -0,0 +1,55 @@
+//Bismillah hir rahmanir rahim
+//keep Patience, Destiny is more beautiful
+
+
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout << "YES"<< nl;
+#define no cout << "NO" << nl;
+#define loop(start, end,var) for (int var = (start); var <= (end); var++)
+#define rsORT(var) sort(var.rbegin(), var.rend())
+#define sORT(var) sort(var.begin(), var.end())
+#define tStr(var) string var; cin >> var;
+#define testCase int t; cin >> t; while(t--)
+
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T> using pbds = tree <T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+   int n;   cin >> n; 
+   vector<int> a(n);
+   int total = 0;
+   for(int i= 0; i < n; i++) {
+        cin >> a [i];
+        total+= a[i];
+   }
+
+   if(total >=0 )
+   {
+    cout << 0 << nl;
+    return;
+   }
+
+   cout << (-total + n -  1) / n << nl;
+}
+int main ()
+{
+    fastIO();
+    testCase{
+
+        solve();
+    }
+    return 0;
+}
