d90c7472b4d42b9c5f786dcbda0870c6150754b2
sadeeabdullah <sadeeabdullah016@gmail.com>
2025-12-29 22:20:05 +0600

    added speed contest 8 problem solution

---
+++ Cp Track/Newbie/Speed contest/contest 8/D_Cab_Rides.cpp
@@ -0,0 +1,53 @@
+//Bismillah hir rahmanir rahim
+//keep Patience, Destiny is more beautiful
+
+
+#include <bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define fastIO() ios::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout << "YES"<< nl;
+#define no cout << "NO" << nl;
+#define loop(start, end,var) for (int var = (start); var <= (end); var++)
+#define rsORT(var) sort(var.rbegin(), var.rend())
+#define sORT(var) sort(var.begin(), var.end())
+#define tStr(var) string var; cin >> var;
+#define testCase int t; cin >> t; while(t--)
+
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T> using pbds = tree <T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+   ll n;    cin >> n;
+   ll cabCount = n / 4;
+   ll extra = n % 4;
+
+   ll ans = cabCount * 400;
+  
+   if(cabCount && extra == 1)
+   ans += 100;
+   else if(extra){
+    ans += max(1LL * 200 , extra * 100);
+   }
+    cout << ans << nl;
+
+}
+int main ()
+{
+    fastIO();
+    testCase{
+
+        solve();
+    }
+    return 0;
+}
