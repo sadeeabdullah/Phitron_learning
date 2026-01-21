6145dec1662df11f02edbb60424eee5b832e859a
sadeeabdullah <sadeeabdullah016@gmail.com>
2026-01-21 23:12:00 +0600

    added div 3 222 solution

---
+++ Cp Track/Super problem solver Zone/CodeChef Starters/Div 3 222/Layered_Cake.cpp
@@ -0,0 +1,52 @@
+// Bismillah hir rahmanir rahim
+// keep Patience, Destiny is more beautiful
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define fastIO()  ios::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout << "YES" << nl;
+#define no cout << "NO" << nl;
+#define loop(start, end, var) for (int var = (start); var <= (end); var++)
+#define rsORT(var) sort(var.rbegin(), var.rend())
+#define sORT(var) sort(var.begin(), var.end())
+#define tStr(var) string var; cin >> var;
+#define testCase  int t; cin >> t;while (t--)
+
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve()
+{
+    ll n, m;    cin >> n >> m;
+    vector<ll> a(n),b(m);
+    for(ll i =0 ;i < n;i++) cin>> a[i];
+    for(ll i =0 ;i < m;i++) cin>> b[i];
+
+    ll ans =0 ;
+    for(ll i=  0;i<  n; i++)
+        for(ll  j = 0; j <  m; j++){
+            // cout << a[i] << " " << b[j] <<  nl;
+            if(a[i]  > b[j])    ans++;
+        }
+    cout << ans  <<   nl;
+}
+int main()
+{
+    fastIO();
+
+    testCase
+    {
+        solve();
+    }
+    return 0;
+}
