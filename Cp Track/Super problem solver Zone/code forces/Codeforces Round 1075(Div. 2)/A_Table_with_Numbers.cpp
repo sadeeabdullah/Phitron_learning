64cb5affd0d4c76228dede20c42d8192ddd1b140
sadeeabdullah <sadeeabdullah016@gmail.com>
2026-01-23 22:36:18 +0600

    added a solution

---
+++ Cp Track/Super problem solver Zone/code forces/Codeforces Round 1075(Div. 2)/A_Table_with_Numbers.cpp
@@ -0,0 +1,59 @@
+// Bismillah hir rahmanir rahim
+// keep Patience, Destiny is more beautiful
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define fastIO()  ios::sync_with_stdio(false); cin.tie(NULL);
+#define yes cout << "YES" << nl;
+#define no cout << "NO" << nl;
+#define loop(start, end, var) for (int var = (start); var <= (end); var++)
+#define rsORT(var) sort(var.rbegin(), var.rend())
+#define sORT(var) sort(var.begin(), var.end())
+#define tStr(var) string var; cin >> var;
+#define testCase  int t; cin >> t;while (t--)
+
+using namespace __gnu_pbds;
+
+using namespace std;
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve(){
+    ll n, h, l; cin >> n >> h>> l;
+    vector<ll> a(n); for(ll i = 0; i  < n; i++) cin >> a[i];
+
+    ll both = 0, r = 0, col = 0;
+    for(auto val : a){
+        if(val <= h && val <= l){
+            both++;
+        }else if(val <= h){
+            r++;
+        }else if(val <= l){
+            col++;
+        }
+    }
+
+    cout << min ((r + col  + both)/ 2 , min(r +both,both+col)) << nl;
+
+
+
+}
+int main()
+{
+    fastIO();
+
+    testCase
+    {
+        solve();
+    }
+    return 0;
+}
+// author : Sadee Abdullah
