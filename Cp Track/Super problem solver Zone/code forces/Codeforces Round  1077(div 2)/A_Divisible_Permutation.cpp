4df57108cc26d2fa57afb6f69d9ccf3239b8123f
sadeeabdullah <sadeeabdullah016@gmail.com>
2026-01-29 22:34:04 +0600

    added first two problem soluiton

---
+++ Cp Track/Super problem solver Zone/code forces/Codeforces Round  1077(div 2)/A_Divisible_Permutation.cpp
@@ -0,0 +1,38 @@
+// Bismillah hir rahmanir rahim
+// keep Patience, Destiny is more beautiful
+
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define rall(var) (var).rbegin(), (var).rend()
+#define all(var) (var).begin(), (var).end()
+#define Ek1takeDekhe9ibo  int t; cin >> t;while (t--)
+#define thakbenaDada return 0
+
+using namespace std;
+
+void EktakeDekhi()
+{
+    ll n;   cin >> n;
+    ll  mid = (n +1) / 2;
+    ll i= mid,l= mid + 1;
+    while(i >= 1 && l <= n){
+        cout << i << " " << l << " ";
+        i--, l++;
+    }
+    if(i != 0) cout << i <<" ";
+    cout << nl;
+}
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    Ek1takeDekhe9ibo
+    {
+        EktakeDekhi();
+    }
+    thakbenaDada;
+}
+/* author : Sadee Abdullah */ 
---
+++ Cp Track/Super problem solver Zone/code forces/Codeforces Round  1077(div 2)/B_Seats.cpp
@@ -0,0 +1,68 @@
+// Bismillah hir rahmanir rahim
+// keep Patience, Destiny is more beautiful
+
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define rall(var) (var).rbegin(), (var).rend()
+#define all(var) (var).begin(), (var).end()
+#define Ek1takeDekhe9ibo  int t; cin >> t;while (t--)
+#define thakbenaDada return 0
+
+using namespace std;
+
+void EktakeDekhi()
+{
+    ll n;   cin >>  n;
+    string s;   cin  >> s;
+    ll zeros  = count(s.begin(),s.end(), '0');
+    ll ones = n - zeros;
+
+    if(n == zeros ){
+        cout << (n + 2) / 3 << nl;
+        return;
+    }
+    // counting untill we get 0
+    ll ans = ones, cnt0 =0;
+
+    ll i = 0;
+    for(; i < n;i++){
+        if(s[i] == '0') cnt0++;
+        else break;
+    }
+
+    if(cnt0 > 0){
+        ans += (cnt0 + 1)/ 3;
+    }
+
+    cnt0 = 0;
+    for(ll j = i + 1; j < n; j++){
+        if(s[j] == '0') cnt0++;
+        else{
+            ans+= cnt0 / 3;
+            cnt0 = 0;
+        }
+    }
+
+
+    if(s[n - 1] == '0'){
+        ans+= (cnt0 + 1) / 3;
+    }
+    cout << ans << nl;
+
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(NULL);
+
+    Ek1takeDekhe9ibo
+    {
+        EktakeDekhi();
+    }
+    thakbenaDada;
+}
+/* author : Sadee Abdullah */ 
