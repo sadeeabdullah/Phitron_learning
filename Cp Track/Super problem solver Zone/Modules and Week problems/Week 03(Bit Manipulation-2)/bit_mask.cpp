d312924b36cbfb6a02f7b524efd0b0a4844556c7
sadeeabdullah <sadeeabdullah016@gmail.com>
2026-02-13 23:41:28 +0600

    added bit mask or getting subsets with bit mask

---
+++ Cp Track/Super problem solver Zone/Modules and Week problems/Week 03(Bit Manipulation-2)/bit_mask.cpp
@@ -0,0 +1,36 @@
+		
+/* Bismillah hir rahmanir rahim */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define rall(var) (var).rbegin(), (var).rend()
+#define all(var) (var).begin(), (var).end()
+const ll Mod = 1e9 + 7;
+
+void Puzzle_Out()
+{
+    int n;	cin >> n;
+    for(int mask = 0; mask < (1 << n); mask++){
+    	cout << mask << " -- > ";
+    	for(int k = 0; k< n; k++){
+    		cout << (((mask >> k ) & 1) ? 1 : 0) << " ";
+    	}
+    	cout << nl;
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    // cin >> t;
+    for (int tc = 1; tc <= t; tc++)
+        Puzzle_Out();
+
+    return 0;
+}
+/* author : Sadee Abdullah */ 
