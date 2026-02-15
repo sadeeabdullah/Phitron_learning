d7b1fd4d406ba3ef791977a7811c884d1cfc6eab
sadeeabdullah <sadeeabdullah016@gmail.com>
2026-02-15 19:27:23 +0600

    added xorwice problem solution'

---
+++ Cp Track/Super problem solver Zone/Modules and Week problems/Week 03(Bit Manipulation-2)/xorwice.cpp
@@ -0,0 +1,34 @@
+/**
+ * problem link : https://codeforces.com/problemset/problem/1421/A
+ * */
+
+		
+/* Bismillah hir rahmanir rahim */
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define llmax LLONG_MAX
+#define nl '\n'
+#define rall(var) (var).rbegin(), (var).rend()
+#define all(var) (var).begin(), (var).end()
+const ll Mod = 1e9 + 7;
+
+void Puzzle_Out()
+{
+    int a, b;	cin >> a >> b;
+    cout << (a^b) << nl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    for (int tc = 1; tc <= t; tc++)
+        Puzzle_Out();
+
+    return 0;
+}
+/* author : Sadee Abdullah */ 
