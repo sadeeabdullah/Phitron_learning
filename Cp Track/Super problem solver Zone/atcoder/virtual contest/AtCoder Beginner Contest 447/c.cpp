a5fe6a4a0fcf4aae7d2f0c8b64f331ae479d6eda
sadeeabdullah <sadeeabdullah016@gmail.com>
2026-03-01 05:25:00 +0600

    added solution of c:

--- Cp Track/Super problem solver Zone/atcoder/virtual contest/AtCoder Beginner Contest 447/c.cpp
+++ Cp Track/Super problem solver Zone/atcoder/virtual contest/AtCoder Beginner Contest 447/c.cpp
@@ -15,18 +15,43 @@ void Puzzle_Out()
     string s, t;
     cin >> s >> t;
     int n=s.size(), m = t.size();
-    int i = 0, j =0;
-    while(i < n && j < m){
-    	if(s[i] == t[j])j++;
-    	i++;
+    std::vector<int> a_a, a_b;
+    string a_ex_s, a_ex_t;
+
+    int cnt =0;
+    for(char c : s){
+        
+        if(c != 'A'){a_ex_s.push_back(c);a_a.push_back(cnt); cnt= 0;}
+        else{
+            cnt++;
+        }
     }
+    a_a.push_back(cnt);
 
-    // if(j < m){
-    // 	cout << -1 << nl;
-    // 	return;
-    // }
+    int cnt2 =0;
+    for(char c : t){
+        if(c != 'A'){a_ex_t.push_back(c);a_b.push_back(cnt2); cnt2= 0;}
+        else{
+            cnt2++;
+        }
+    }
+    a_b.push_back(cnt2);
 
-    cout << (n - j) + (m - j) << nl;
+    if(a_ex_s != a_ex_t){
+        cout <<-1<<nl;
+        return;
+    }   
+    // for(auto val : a_a)cout<<val<<" ";
+    // cout<<nl;
+    // for(auto val : a_b)cout<<val<<" ";
+    // cout<<nl;
+
+    int ans = 0 ;
+    for (int i = 0; i < a_a.size(); ++i)
+    {
+        ans+= abs(a_a[i] -  a_b[i]);
+    }
+    cout<<ans<<nl;
 }
 int main()
 {
